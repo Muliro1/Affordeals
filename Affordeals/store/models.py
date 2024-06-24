25d5b265d3da7a4cbeed2c33d6a268714d9aa339
EmmanuelAdu <christorenzo20@gmail.com>
2024-06-24 18:49:30 +0000

    Resolve database model issues, Django migration errors

--- Affordeals/store/models.py
+++ Affordeals/store/models.py
@@ -6,8 +6,8 @@ class SiteUser(models.Model):
     """
     Model representing a user
     """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, default='')
     email = models.EmailField(max_length=255)
     phone_number = models.CharField(max_length=20)
     password = models.CharField(max_length=100)
@@ -19,7 +19,7 @@ class Address(models.Model):
     """
     Model representing user address
     """
-    user_id = ForeignKey(SiteUser, on_delete=CASCADE)
+    user_id = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
     country = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
@@ -39,11 +39,11 @@ class Products(models.Model):
     Model representing products
     """
     name = models.CharField(max_length=100)
-    category_id = ForeignKey(Category, on_delete=models.PROTECT)
+    category_id = models.ForeignKey(Category, on_delete=models.PROTECT)
     image = models.ImageField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digit=6, decimal_places=2)
-    quantity_in_stock = models.IntegerField(max_digit=6)
-    discount = models.DecimalField(max_digit=3, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity_in_stock = models.IntegerField()
+    discount = models.DecimalField(max_digits=3, decimal_places=2)
     # options = models.
 
