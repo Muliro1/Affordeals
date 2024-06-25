885d50ebd7df6a8109e1623e5795d20dd8599d4e
Muliro1 <mulirokhaemba@gmail.com>
2024-06-25 13:26:11 +0300

    edit

--- Affordeals/users/views.py
+++ Affordeals/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.http import HttpResponse
 
 def register(request):
     if request.method == 'POST':
@@ -8,7 +9,7 @@ def register(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('admin/')
+            return HttpResponse('<h1> account created for user</h1>')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
