1ad9c46a1e6e2eeb51af5e397aabc92d790f075f
Nathanage3 <sisay954@gmail.com>
2024-06-26 20:14:46 +0300

    some updates

--- Affordeals/store/models.py
+++ Affordeals/store/models.py
@@ -1,25 +1,36 @@
+from django.contrib import admin
+from django.conf import settings
 from django.db import models
 
 
-# Create your models here.
 class SiteUser(models.Model):
     """
     Model representing a user
     """
-    first_name = models.CharField(max_length=100, default='')
-    last_name = models.CharField(max_length=100, default='')
-    email = models.EmailField(max_length=255)
     phone_number = models.CharField(max_length=20)
-    password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return '{} {}'.format(self.user.first_name, self.user.last_name)
+    
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
 
 class Address(models.Model):
     """
     Model representing user address
     """
-    user_id = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
+    siteuser = models.ForeignKey(SiteUser, on_delete=models.CASCADE)
     country = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
@@ -32,6 +43,12 @@ class Category(models.Model):
     Model representing products categories
     """
     name = models.CharField(max_length=100)
+    highlighted_product = models.ForeignKey(
+        'Products', on_delete=models.SET_NULL,
+        null = True,
+        related_name='+',
+        blank=True
+    )
 
 
 class Products(models.Model):
@@ -39,11 +56,37 @@ class Products(models.Model):
     Model representing products
     """
     name = models.CharField(max_length=100)
-    category_id = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     image = models.ImageField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity_in_stock = models.IntegerField()
-    discount = models.DecimalField(max_digits=3, decimal_places=2)
-    # options = models.
 
+class Order(models.Model):
+    """
+    Order model.
+    """
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+    created_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1,
+                        choices=PAYMENT_STATUS_CHOICES,
+                        default=PAYMENT_STATUS_PENDING)
+    siteuser = models.ForeignKey(SiteUser, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    """
+    OrderItem Model.
+    """
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    products = models.ForeignKey(Products, on_delete=models.PROTECT, related_name='orderitems')
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
