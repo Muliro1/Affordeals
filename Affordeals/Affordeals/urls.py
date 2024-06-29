5a3741c63ae43fe9e7ef8016ddff9d01f4386ac4
Muliro1 <mulirokhaemba@gmail.com>
2024-06-29 16:17:21 +0300

    landing page

--- Affordeals/Affordeals/urls.py
+++ Affordeals/Affordeals/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', user_views.home, name='home'),
     path('store/', include('store.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt'))
--- Affordeals/main/views.py
+++ Affordeals/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def home(request):
+    form = UserCreationForm()
+    return render(request, 'main/index.html', {'form': form})
+
