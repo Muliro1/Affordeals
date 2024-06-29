37ff650049d775afeba5e8291b426df93de391ff
Muliro1 <mulirokhaemba@gmail.com>
2024-06-29 22:27:28 +0300

    save-to-db

--- Affordeals/main/views.py
+++ Affordeals/main/views.py
@@ -8,6 +8,7 @@ def home(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('login')
