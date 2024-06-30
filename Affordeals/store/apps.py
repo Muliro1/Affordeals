dd4992238a6fb7cc9adcf216e81c3840a859d9b3
EmmanuelAdu <christorenzo20@gmail.com>
2024-06-30 17:47:10 +0000

    Add detailed comments to admin classes and StoreConfig for better documentation

--- Affordeals/store/apps.py
+++ Affordeals/store/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class StoreConfig(AppConfig):
+    """
+    Configuration for the Store application.
+
+    Attributes:
+    - default_auto_field (str): Specifies the type of auto-generated field to
+      use for primary keys.
+    - name (str): The name of the application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
