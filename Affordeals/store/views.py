893e945814007593b3909dac3619bc00022a7202
EmmanuelAdu <christorenzo20@gmail.com>
2024-06-30 18:04:45 +0000

    Add detailed comments to viewset classes for better documentation

--- Affordeals/store/views.py
+++ Affordeals/store/views.py
@@ -11,6 +11,19 @@ from store.permissions import IsAdminOrReadOnly, FullPermissions
 
 
 class SiteUserViewSet(ModelViewSet):
+  """
+    A viewset for viewing and editing SiteUser instances.
+
+    Attributes:
+    - queryset (QuerySet): The queryset for retrieving SiteUser instances.
+    - serializer_class (Serializer): The serializer class to use
+      for SiteUser instances.
+    - permission_classes (list): The permission classes to apply to this viewset.
+
+    Actions:
+    - me (function): A custom action to retrieve or update the authenticated
+      user's site user profile.
+  """
   queryset = SiteUser.objects.all()
   serializer_class = SiteUserSerializer
   permission_classes = [IsAdminUser]
@@ -18,6 +31,14 @@ class SiteUserViewSet(ModelViewSet):
 
   @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
   def me(self, request):
+    """
+      Retrieve or update the authenticated user's site user profile.
+
+      Methods:
+      - GET: Returns the authenticated user's site user profile.
+      - PUT: Updates the authenticated user's site user profile
+        with the provided data.
+    """
     (siteuser, created) = SiteUser.objects.get_or_create(user_id=request.user.id)
     if request.method == 'GET':
       serializer = SiteUserSerializer(siteuser)
@@ -30,23 +51,65 @@ class SiteUserViewSet(ModelViewSet):
       return Response(serializer.data)
 
 class ProductsViewSet(ModelViewSet):
+  """
+    A viewset for viewing and editing Product instances.
+
+    Attributes:
+    - queryset (QuerySet): The queryset for retrieving Product instances.
+    - serializer_class (Serializer): The serializer class to use
+      for Product instances.
+    - permission_classes (list): The permission classes to apply
+      to this viewset.
+  """
   queryset = Products.objects.all()
   serializer_class = ProductsSerializer
   permission_classes = [IsAdminOrReadOnly]
 
 
 class CategoryViewSet(ModelViewSet):
+  """
+    A viewset for viewing and editing Category instances.
+
+    Attributes:
+    - queryset (QuerySet): The queryset for retrieving Category instances.
+    - serializer_class (Serializer): The serializer class to use for
+      Category instances.
+    - permission_classes (list): The permission classes to apply to
+      this viewset.
+  """
   queryset = Category.objects.all()
   serializer_class = CategorySerializer
   permission_classes = [IsAdminOrReadOnly]
 
 class ShoppingOrderViewSet(ModelViewSet):
+  """
+    A viewset for viewing and editing ShoppingOrder instances.
+
+    Attributes:
+    - queryset (QuerySet): The queryset for retrieving ShoppingOrder
+      instances.
+    - serializer_class (Serializer): The serializer class to use for
+      ShoppingOrder instances.
+    - permission_classes (list): The permission classes to apply to
+      this viewset.
+  """
   queryset = ShoppingOrder.objects.all()
   serializer_class = ShoppingOrderSerializer
   permission_classes = [IsAuthenticated]
 
 
 class ShoppingOrderItemViewSet(ModelViewSet):
+  """
+    A viewset for viewing and editing ShoppingOrderItem instances.
+
+    Attributes:
+    - queryset (QuerySet): The queryset for retrieving ShoppingOrderItem
+      instances.
+    - serializer_class (Serializer): The serializer class to use for
+      ShoppingOrderItem instances.
+    - permission_classes (list): The permission classes to apply to
+      this viewset.
+  """
   queryset = ShoppingOrderItem.objects.all()
   serializer_class = ShoppingOrderItemSerializer
   permission_classes = [IsAuthenticated]
