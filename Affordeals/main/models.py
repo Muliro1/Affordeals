80ae84d61de0351def81cbbdd63f6c3a31a0acaa
Muliro1 <mulirokhaemba@gmail.com>
2024-07-01 14:21:34 +0300

    comments

--- Affordeals/main/models.py
+++ Affordeals/main/models.py
@@ -2,12 +2,35 @@ from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 from django.contrib.auth import get_user_model
 
-class User(AbstractUser):
-  email = models.EmailField(unique=True)
+# Class User:
+#   Extends the AbstractUser model from django.contrib.auth.models
+#   Adds an email field which is unique
+#
+# Fields:
+#   email = models.EmailField(unique=True)
+
+# This is a custom user model that extends the AbstractUser model provided by Django.
+# It adds a profile image field to the user model.
 
 class profile(models.Model):
+  """
+  Model representing a user profile.
+  
+  Fields:
+  - user (OneToOneField): A one-to-one relationship with the Django user model.
+  - image (ImageField): A field to store the user's profile image.
+  
+  Methods:
+  - __str__(): Returns the username of the user.
+  """
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+  
   def __str__(self):
+    """
+    Returns the username of the user.
+    
+    Returns:
+    - str: The username of the user.
+    """
     return f'{self.user.username} Profile'
-
--- Affordeals/main/signals.py
+++ Affordeals/main/signals.py
@@ -3,11 +3,35 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import User, profile
 
+
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    """
+    Create a profile for the user when a new user is created.
+
+    Args:
+        sender: The sender of the signal.
+        instance: The instance of the User model.
+        created: A boolean indicating whether the User instance was created.
+        **kwargs: Additional keyword arguments.
+    """
+    # Check if the user instance was created
     if created:
+        # Create a profile for the user
         profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
+    """
+    Save the profile associated with the user instance.
+
+    Args:
+        sender: The sender of the signal.
+        instance: The instance of the User model.
+        **kwargs: Additional keyword arguments.
+    """
+    # Save the profile associated with the user instance
+    # This is necessary to ensure that the profile is saved
+    # whenever the user instance is saved
     instance.profile.save()
