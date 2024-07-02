750aa48085adf7cdfb7ce6b4ffce8cb696dbc985
Muliro1 <mulirokhaemba@gmail.com>
2024-07-02 21:03:10 +0300

    landing-page-route-fix

--- Affordeals/Affordeals/urls.py
+++ Affordeals/Affordeals/urls.py
@@ -25,8 +25,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', user_views.home, name='home'),
-    #path('login/', user_views.login, name='login'),
+    path('', user_views.home, name='home'),
     path('account/', user_views.account, name='account'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
