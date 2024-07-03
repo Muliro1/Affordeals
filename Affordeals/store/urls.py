fccdaf74e18ced3e55ff47260edc21a917ea81b0
Nathanage3 <sisay954@gmail.com>
2024-07-03 19:56:44 +0300

    store/urls.py

---
+++ Affordeals/store/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include
+from rest_framework_nested import routers
+from .views import SiteUserViewSet, ProductsViewSet, CategoryViewSet, ShoppingOrderViewSet,\
+                   ShoppingOrderItemViewSet, ShoppingCartItemViewSet, ShoppingCartViewSet
+
+
+router = routers.DefaultRouter()
+router.register('product', ProductsViewSet, basename='product')
+router.register('category', CategoryViewSet)
+router.register('orders', ShoppingOrderViewSet, basename='orders')
+
+router.register('carts', ShoppingCartViewSet, basename='carts')
+router.register('siteuser', SiteUserViewSet)
+
+
+product_router = routers.NestedDefaultRouter(router, 'product', lookup='products')
+product_router.register('reviews', ReviewViewSet, basename='products-reviews' )
+
+cartRouter = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+cartRouter.register('cartitems', ShoppingCartItemViewSet, basename='cart-cartitems')
+
+
+urlpatterns = [
+  path('', include(router.urls)),
+  path('', include(product_router.urls)),
+  path('', include(cartRouter.urls)),
+]
