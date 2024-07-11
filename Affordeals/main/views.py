5480e6c5aa55a0c59e0e422e83d5fbf5bb6a3cb7
Nathanage3 <sisay954@gmail.com>
2024-07-11 11:30:05 +0300

    update home

--- Affordeals/main/views.py
+++ Affordeals/main/views.py
@@ -9,25 +9,42 @@ from django.core.paginator import Paginator
 from store.serializers import ProductsSerializer
 from rest_framework.viewsets import ModelViewSet
 from store.permissions import IsAdminOrReadOnly
-# Create your views here.
 
-def home(request):
+
+### Testing ###
+def register(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!, you are now able to login')
+            messages.success(request, f'Your account has been created! You are now able to log in, {username}!')
             return redirect('login')
+        else:
+            print("Form is not valid")
+            print(form.errors)  # Print form errors for debugging
     else:
-        form = CustomUserCreationForm(request.POST)
-    return render(request, 'main/index.html', {'form': form})
+        form = CustomUserCreationForm()
+    return render(request, 'main/register.html', {'form': form})
+
+
+def home(request):
+    # if request.method == 'POST':
+    #     form = CustomUserCreationForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         username = form.cleaned_data.get('username')
+    #         messages.success(request, f'Account created for {username}!, you are now able to login')
+    #         return redirect('login')
+    # else:
+    #     form = CustomUserCreationForm(request.POST)
+    return render(request, 'main/home.html')
 
 def about(request):
     return render(request, 'main/about.html')
     
 
-@login_required
+
 def product_view(request):
     products = Products.objects.all()
     ordered_products = products.order_by('category_id')
